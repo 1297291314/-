1c264de55b8cde99d2f52ebe4b083a64151eb0c2
hujunjie <1297291314@qq.com>
2023-04-26 17:41:58 +0800

    修改

--- pages/GPT/GPT.js
+++ pages/GPT/GPT.js
@@ -53,7 +53,9 @@ Page({
     })
   },
   onLoad:function(){
-    this.setData({audioContext:wx.createInnerAudioContext()})
+    this.setData({audioContext:wx.createInnerAudioContext({
+      useWebAudioImplement: false // 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项
+    })})
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
